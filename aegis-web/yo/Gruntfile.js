55abfb23c4933e69e1f7aaa946fe0a2f638eb9b5
Christos Botsikas <cbotsikas@gmail.com>
2019-06-11 12:52:26 +0200

    Enable proxy for build release testing
    You can locally test the build release using:
    grunt serve:dist

--- aegis-web/yo/Gruntfile.js
+++ aegis-web/yo/Gruntfile.js
@@ -489,7 +489,11 @@ module.exports = function (grunt) {
 
   grunt.registerTask('serve', 'Compile then start a connect web server', function (target) {
     if (target === 'dist') {
-      return grunt.task.run(['build', 'connect:dist:keepalive']);
+      return grunt.task.run([
+        'build', 
+        'configureProxies', 
+        'connect:dist:keepalive'
+      ]);
     }
 
     grunt.task.run([
