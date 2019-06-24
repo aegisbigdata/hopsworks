473739c33d82021f2e417d3e25e6e85ae1c41b8f
Christoph Müller <christoph.mueller@fokus.fraunhofer.de>
2019-06-24 14:27:52 +0200

    Add default env file

---
+++ aegis-web/yo/app/scripts/env/default.js
@@ -0,0 +1,8 @@
+angular.module('hopsWorksApp').constant('AEGIS_CONFIG', {
+  aegis_metadata_api: {
+    PROJECT_ENDPOINT: 'https://bbc6.sics.se:8181/hopsworks-api/aegis-metadata/catalogues/',
+    DATASET_ENDPOINT: 'https://bbc6.sics.se:8181/hopsworks-api/aegis-metadata/datasets/'
+    DISTRIBUTION_ENDPOINT: 'https://bbc6.sics.se:8181/hopsworks-api/aegis-metadata/distributions/',
+    API_KEY: ''
+  }
+});
