4fc3d46fd5ce28497521085d0254caec9f2064a8
Christos Botsikas <cbotsikas@gmail.com>
2019-06-27 07:17:57 +0200

    Basic environment configuration via config files for different environments - Use yo/env/ to store various configuration environments based on dev. - You can switch environment using: grunt config[:<env>], default is dev - The <env> parameter is the name of the folder inside yo/env/. For example you can create a folder named "prod". To use that run: grunt config:prod - You need to run it at least once. - yo/app/scripts/config.js is ignored by git and only modified using grunt config command

---
+++ aegis-web/yo/env/dev/config.js
@@ -0,0 +1,9 @@
+var AEGIS_CONFIG;
+(function (AEGIS_CONFIG) {
+    AEGIS_CONFIG.metadata = {
+        API_KEY: '',
+        CATALOGUE_ENDPOINT: '/hopsworks-api/aegis-metadata/catalogues/',
+        DATASET_ENDPOINT: '/hopsworks-api/aegis-metadata/datasets/',
+        DISTRIBUTION_ENDPOINT: '/hopsworks-api/aegis-metadata/distributions/'
+    };
+})(AEGIS_CONFIG || (AEGIS_CONFIG = {}));
