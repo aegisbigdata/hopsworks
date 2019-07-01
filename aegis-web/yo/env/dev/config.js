308ce65468d7330d7f07053a01dc9b4affce73c6
Fabian Kirstein <fabian.kirstein@fokus.fraunhofer.de>
2019-07-01 15:52:01 +0200

    Correct Config for Metadata Service

--- aegis-web/yo/env/dev/config.js
+++ aegis-web/yo/env/dev/config.js
@@ -1,9 +1,9 @@
 var AEGIS_CONFIG;
 (function (AEGIS_CONFIG) {
     AEGIS_CONFIG.metadata = {
-        API_KEY: '',
-        CATALOGUE_ENDPOINT: '/hopsworks-api/aegis-metadata/catalogues/',
-        DATASET_ENDPOINT: '/hopsworks-api/aegis-metadata/datasets/',
-        DISTRIBUTION_ENDPOINT: '/hopsworks-api/aegis-metadata/distributions/'
+        API_KEY: '895b69ae-7093-11e9-a923-1681be663d3e',
+        CATALOGUE_ENDPOINT: 'https://bbc6.sics.se:8181/hopsworks-api/aegis-metadata/catalogues/',
+        DATASET_ENDPOINT: 'https://bbc6.sics.se:8181/hopsworks-api/aegis-metadata/datasets/',
+        DISTRIBUTION_ENDPOINT: 'https://bbc6.sics.se:8181/hopsworks-api/aegis-metadata/distributions/'
     };
-})(AEGIS_CONFIG || (AEGIS_CONFIG = {}));
+})(AEGIS_CONFIG || (AEGIS_CONFIG = {}));git st
