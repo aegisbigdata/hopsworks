7794f5e4cffafe05771fef9093e07cb5949b50ec
Christos Botsikas <cbotsikas@gmail.com>
2019-07-01 17:44:17 +0200

    Fix request interceptor api url detection

--- aegis-web/yo/env/dev/config.js
+++ aegis-web/yo/env/dev/config.js
@@ -2,8 +2,8 @@ var AEGIS_CONFIG;
 (function (AEGIS_CONFIG) {
     AEGIS_CONFIG.metadata = {
         API_KEY: '895b69ae-7093-11e9-a923-1681be663d3e',
-        CATALOGUE_ENDPOINT: 'https://bbc6.sics.se:8181/hopsworks-api/aegis-metadata/catalogues/',
-        DATASET_ENDPOINT: 'https://bbc6.sics.se:8181/hopsworks-api/aegis-metadata/datasets/',
-        DISTRIBUTION_ENDPOINT: 'https://bbc6.sics.se:8181/hopsworks-api/aegis-metadata/distributions/'
+        CATALOGUE_ENDPOINT: '/hopsworks-api/aegis-metadata/catalogues/',
+        DATASET_ENDPOINT: '/hopsworks-api/aegis-metadata/datasets/',
+        DISTRIBUTION_ENDPOINT: '/hopsworks-api/aegis-metadata/distributions/'
     };
 })(AEGIS_CONFIG || (AEGIS_CONFIG = {}));
